34c593df7256dcfc31fd5dcb17ebf5a66d8c233f
siriusgowi <siriusgowi@gmail.com>
2021-11-01 19:53:05 +0530

    worked out useEffects

--- src/tutorial/2-useEffect/setup/1-useEffect-basics.js
+++ src/tutorial/2-useEffect/setup/1-useEffect-basics.js
@@ -3,7 +3,23 @@ import React, { useState, useEffect } from 'react';
 // cleanup function
 // second parameter
 const UseEffectBasics = () => {
-  return <h2>useEffect Basics</h2>;
+  const [value, setValue] = useState(0)
+
+
+  useEffect(()=>{
+    if(value >= 1){
+    document.title= `New Messages (${value})`
+    }
+    return console.log('Condition values cannot applicable outside of the useEffect')
+  })
+
+  console.log('first rendered')
+
+  return (<div className='container'>
+  <h1>{value}</h1>
+  <button type='button' className='btn' onClick={()=> setValue(value + 1)}>click me</button>
+  
+  </div>)
 };
 
 export default UseEffectBasics;
--- src/tutorial/2-useEffect/setup/2-useEffect-cleanup.js
+++ src/tutorial/2-useEffect/setup/2-useEffect-cleanup.js
@@ -4,7 +4,27 @@ import React, { useState, useEffect } from 'react';
 // second argument
 
 const UseEffectCleanup = () => {
-  return <h2>useEffect cleanup</h2>;
+  const [size , setSize] = useState(window.innerWidth)
+ 
+const checkSize = ()=>{
+ setSize(window.innerWidth)
+}
+
+  useEffect(()=>{
+    console.log('useEffect')
+    window.addEventListener('resize', checkSize)
+    return ()=>{
+      console.log('checkSize')
+     window.removeEventListener('resize', checkSize)
+    }
+  })
+
+  return (
+    <div className='container'>
+  <h2>useEffect cleanup</h2>
+  <h3>{size} PX</h3>
+  </div>
+  );
 };
 
 export default UseEffectCleanup;
