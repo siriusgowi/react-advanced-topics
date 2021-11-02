577324cd30e984a223c65abfd521516794adb870
siriusgowi <siriusgowi@gmail.com>
2021-11-02 19:54:01 +0530

    updated conditional rendering

--- src/tutorial/3-conditional-rendering/setup/1-multiple-returns.js
+++ src/tutorial/3-conditional-rendering/setup/1-multiple-returns.js
@@ -1,7 +1,19 @@
 import React, { useState, useEffect } from 'react';
 const url = 'https://api.github.com/users/QuincyLarson';
 const MultipleReturns = () => {
-  return <h2>multiple returns</h2>;
+
+  const [isLoading, setIsLoading] = useState(true)
+
+  if(isLoading){
+    return <div className='container'>
+    <h1>Loading...</h1>
+    </div>
+  }
+  return (
+    <div className='container'>
+    <h1>conditional rendering</h1>
+    </div>
+  );
 };
 
 export default MultipleReturns;
